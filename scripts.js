7a1f15c2e0f5bec21ca18eefa64996c994bd1c4a
ca-Stella <stella.2532@gmail.com>
2022-07-19 12:28:57 -0600

    Create basic math functions and test on console

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+function add(x, y) {
+    z = x + y;
+    console.log(z);
+}
+
+function minus(x, y) {
+    z = ((x*100000000) - (y*100000000))/100000000;
+    console.log(z);
+}
+
+function multiply(x, y) {
+    z = x * y;
+    console.log(z);
+}
+
+function divide(x, y) {
+    z = x / y;
+    console.log(z);
+}
